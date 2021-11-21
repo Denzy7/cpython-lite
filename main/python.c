a3b0f55c29fe58a39b639a0a216f868b83bad250
Dennis <33323861+Denzy7@users.noreply.github.com>
2021-11-21 19:51:25 +0300

    Fix weird vim indentation and space quirks

--- main/python.c
+++ main/python.c
@@ -6,10 +6,10 @@
 
 void init_py()
 {
-	//Python flags to prevent loading some unneeded stuff
-	Py_NoSiteFlag = 1;
-	Py_FrozenFlag = 1;
-	Py_IgnoreEnvironmentFlag = 1;
+   //Python flags to prevent loading some unneeded stuff
+    Py_NoSiteFlag = 1;
+    Py_FrozenFlag = 1;
+    Py_IgnoreEnvironmentFlag = 1;
 
     printf("loading libs\n");
 	//Try to create a test file to see if directory is writable
@@ -18,11 +18,11 @@ void init_py()
     
     FILE* fff = fopen(bootstrap_d2, "wb");
 
-if(!fff)
-{
+    if(!fff)
+    {
 	printf("Unable to create a mock file in %s. Ensure the directory is writable\n", bootstrap_d2);
 	exit(1);
-}
+    }
 
     const char* text = "Hello\n\0";
     fwrite(text, 1, sizeof(text), fff);
@@ -37,7 +37,8 @@ if(!fff)
 	//Start interprator
     Py_Initialize();
 
-//Import hook for embedding python and wanting to hardcode packages (optional)
+   //Import hook for embedding python and wanting to hardcode packages (optional)
+   //Will fail (but not crash python) if no importlib was baked into cpython lib
     PyRun_SimpleString(
         "import importlib.abc\n" \
         "import importlib.machinery\n" \
@@ -55,18 +56,14 @@ if(!fff)
         "\n" \
         "sys.meta_path.append(Finder())\n" \
     );
-
-
-
 }
-
 
 #ifdef MS_WINDOWS
 int
 main(int argc, wchar_t **argv)
 {
 
-init_py();
+    init_py();
 	
     return Py_Main(argc, argv);
 }
@@ -74,7 +71,7 @@ init_py();
 int
 main(int argc, char **argv)
 {
-init_py();
+    init_py();
 	
     return Py_BytesMain(argc, argv);
 }
